fc32103af2470db67aaffb6659114f8e9f77e8d0
Kevin Hester <kevinh@geeksville.com>
2014-01-16 11:54:35 -0800

    add size tests for a full gps pos msg

--- mavlink2/build.sh
+++ mavlink2/build.sh
@@ -2,3 +2,4 @@ mkdir -p pytest
 /usr/bin/protoc --python_out=pytest mavlink.proto
 /usr/bin/protoc --encode=Envelope mavlink.proto <hbtest.txt >hbtest.bin
 /usr/bin/protoc --encode=Envelope mavlink.proto <gpsshorttest.txt >gpsshorttest.bin
+/usr/bin/protoc --encode=Envelope mavlink.proto <gpslongtest.txt >gpslongtest.bin
