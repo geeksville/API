ab8d993e698451fcd59a80314e481b43c845f073
Kevin Hester <kevinh@geeksville.com>
2014-03-20 09:31:04 -1000

    fix doc script

--- update-docs.sh
+++ update-docs.sh
@@ -3,12 +3,13 @@ exit
 FIXME make sure we wont run if git dir not clean or if we have faults
 cd docs
 make html
+rm -r /tmp/autodocs
 cp -a _build/html /tmp/autodocs
 cd ..
-git checkout gh_pages
+git checkout gh-pages
 cp -a /tmp/autodocs/* .
 find . | xargs git add
-git commit -c "Update docs"
+git commit -m "Update docs"
 git push
 git checkout master
 
